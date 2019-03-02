b6c9c71331111a721ccd2240926aa4fb9c871e50
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-03-01 23:14:41 -0500

    20190301 update

---
+++ bamazonCustomer.js
@@ -0,0 +1,118 @@
+// TIME SPENT:
+// 3/1 2:15
+// 3/2 
+
+// 5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+
+// 6. The app should then prompt users with two messages.
+
+//    * The first should ask them the ID of the product they would like to buy.
+//    * The second message should ask how many units of the product they would like to buy.
+
+// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
+
+//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
+
+// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
+//    * This means updating the SQL database to reflect the remaining quantity.
+//    * Once the update goes through, show the customer the total cost of their purchase.
+
+/* 
+X - connect to db
+X - pull the full db over into an array/objects
+X - barf it out to the terminal
+X - inquire "what product would you like to buy"
+X - inquire "how many would you like to buy"
+X - compare # ordered against quantity in the object
+X - logic for having enough quantity or not
+8 - update db to remove quantity
+9 - show total cost of purchase
+10 - ask whether they want to keep shopping
+11 - exit or reload the inquirer trigger
+*/
+
+
+// TODO: need to have some sort of data input validation happening
+
+// =============================================
+// NPM PACKAGES
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const pad = require('pad');
+// TODO: should I remove npm pad from this project?
+// =============================================
+
+
+// // =============================================
+// CONNECT TO DB, GET PRODUCTS
+// set connection object
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "uncb00tc4mp",
+    database: "bamazon"
+});
+// actually connect
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connction id: " + connection.threadId);
+    // run your query
+    connection.query('select * from products', function(err, data) {
+        if (err) throw err;
+        // display the data
+        console.log(data);
+        // TODO: why does a FOREACH not work here?
+        // data.foreach(function(item){
+        //     console.log(item.item_id);
+        //     console.log(item.product_name);
+        // });
+        // TODO: ^^^ why does a FOREACH not work here?
+        console.log("\n\nAVAILABLE ITEMS:")
+        console.log("\nItem ID | Name | Category | Price")
+        // TODO: need to figure out a way to make the "X.00" prices actually print this way (instead of stripping out the empty decimals)
+        for (i=0;i<data.length;i++) {
+            console.log(`${data[i].item_id} | ${data[i].product_name} | ${data[i].department_name} | $${data[i].price}`);
+        }
+        console.log("");
+        // get user input
+        inquirer.prompt([
+            {
+                // TODO: can inquirer be setup to only accept numbers?
+                type: "input",
+                message: "Enter the Item ID of the product you would like to purchase.",
+                name: "buyID"
+            },
+            {
+                type: "input",
+                message: "How many would you like to purchase?",
+                name: "buyQuantity"
+            }
+        ]).then(function(response) {
+            // is there enough stock?
+            console.log("Wants to buy " + response.buyQuantity + " of Item ID " + response.buyID + ".");
+            // console.log("There are " + data[response.buyID].stock_quantity + " in stock.");
+            // TODO: ^^ this doesn't work b/c we're looking at the array entry corresponding with the response.buyID, not the array entry containing an object with the item_ID corresponding to response.buyID.
+            // TODO: could do a second query to the DB, but that seems too.... something.
+            // console.log("response.buyID: " + response.buyID);
+            // console.log("All item IDs:");
+            for (q=0;q<data.length;q++) { // loop through the array of objects
+                console.log(data[q].item_id);
+                if (data[q].item_id == response.buyID) { // look for a match between user's ID input and the item_ID of the object
+                // ^^ if you do a === match, you need to convert the user input into a number
+                    console.log("found a match!");
+                    if (data[q].stock_quantity < response.buyQuantity) {
+                        console.log("Sorry, we do not have that many in stock.");
+                    } else {
+                        console.log("Sold!")
+                    }
+                } 
+            }
+        });
+        // close the connection
+        connection.end();
+    });
+
+});
+// =============================================
+
---
+++ create_db.sql
@@ -0,0 +1,29 @@
+create database bamazon;
+use bamazon;
+
+create table products (
+	item_id int auto_increment not null,
+    product_name varchar(50),
+    department_name varchar(50),
+    price decimal(10,2),
+    stock_quantity integer,
+    primary key (item_id)
+);
+
+insert into products (product_name,department_name,price,stock_quantity) values 
+("Gibson Thunderbird Bass","Musical Instruments",2299.00,23),
+("Fender Jimmy Page Telecaster","Musical Instruments",1399.99,16),
+("Schecter Baron-H Vintage Bass","Musical Instruments",679.00,5),
+("Interpol - The Marauder","Vinyl",21.99,325),
+("The Afghan Whigs - In Spades","Vinyl",18.95,175),
+("Blonde Redhead - Penny Sparkle","Vinyl",19.49,103),
+("Hall & Oates - Greatest Hits","Vinyl",24.99,50),
+("Hot Wheels Nissan R32 Skyline","Toys",5.49,24),
+("Hot Wheels 71 Porsche 911","Toys",0.94,423),
+("Hot Wheels Tesla Roadster","Toys",0.97,212);
+
+select * from products;
+
+
+
+
