8d8180b1ab733b27745f37cbf21d4d9ab48775a2
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-03-02 23:39:15 -0500

    20190302 working version, base assignment

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,42 +1,6 @@
 // TIME SPENT:
 // 3/1 2:15
-// 3/2 0:50
-// 3/2 
-
-
-// 5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
-
-// 6. The app should then prompt users with two messages.
-
-//    * The first should ask them the ID of the product they would like to buy.
-//    * The second message should ask how many units of the product they would like to buy.
-
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//    * This means updating the SQL database to reflect the remaining quantity.
-//    * Once the update goes through, show the customer the total cost of their purchase.
-
-/* 
-X - connect to db
-X - pull the full db over into an array/objects
-X - barf it out to the terminal
-X - inquire "what product would you like to buy"
-X - inquire "how many would you like to buy"
-X - compare # ordered against quantity in the object
-X - logic for having enough quantity or not
-X - update db to remove quantity
-X - show total cost of purchase
-X - ask whether they want to keep shopping
-TODO: 11 - exit or reload the inquirer trigger
-TODO: give them a way to get out of the app by entering Q
-*/
-
-
-// TODO: need to have some sort of data input validation happening
-// TODO: ALSO set my queries to sanitize user input
+// 3/2 2:50
 
 // =============================================
 // NPM PACKAGES
@@ -52,7 +16,7 @@ const chalk = require('chalk');
 // =============================================
 // GLOBALS
 var updateQuantity;
-var updateID;
+var availableItems = [];
 // =============================================
 
 
@@ -69,114 +33,130 @@ var connection = mysql.createConnection({
 
 
 // =============================================
-// customer() FUNCTION
+// CREATE customer() FUNCTION
 var customer = function() {
-    // connect to DB
-    connection.connect(function(err){
-        if(err) throw err;
-        // run our query
-        connection.query('select * from products', function(err, data) {
-            if (err) throw err;
-            console.log(chalk.red.bold("\n============================================="));
-            console.log(chalk.red.bold("||          WELCOME TO BOOTCAMPAZON        ||"));
-            console.log(chalk.red.bold("=============================================\n"));
-            // display the data w/ table package
-            let output;
-            let tableData = [];
-            tableData.push([
-                chalk.yellow.bold("ID"),
-                chalk.yellow.bold("Product"),
-                chalk.yellow.bold("Department"),
-                chalk.yellow.bold("In Stock"),
-                chalk.yellow.bold("Price")
-            ]);
-            for (i=0;i<data.length;i++) {
-                var newArray = [
-                    data[i].item_id,
-                    data[i].product_name,
-                    data[i].department_name,
-                    data[i].stock_quantity,
-                    data[i].price.toFixed(2)
-                ];
-                tableData.push(newArray);
-            }
-            output = table(tableData);
-            console.log(output);
-            // get user input
-            inquirer.prompt([
-                {
-                    type: "number",
-                    message: "Enter the Item ID of the product you would like to purchase.",
-                    name: "buyID",
-                    // validate: function validateInt(input) {
-
-                    // }
+    // clear availableItems[]:
+    availableItems = [];
+    // run our query
+    connection.query('select * from products', function(err, data) {
+        if (err) throw err;
+        console.log(chalk.red.bold("\n============================================="));
+        console.log(chalk.red.bold("||           BUY FROM BOOTCAMPAZON         ||"));
+        console.log(chalk.red.bold("=============================================\n"));
+        // display the data w/ table package
+        let output;
+        let tableData = [];
+        config = {
+            columns: {
+                0: {
+                    alignment: 'left',
                 },
-                {
-                    type: "input",
-                    message: "How many would you like to purchase?",
-                    name: "buyQuantity"
+                1: {
+                    alignment: 'left',
+                },
+                2: {
+                    alignment: 'left',
+                },
+                3: {
+                    alignment: 'left',
+                },
+                4: {
+                    alignment: 'right',
                 }
-            ]).then(function(response) {
-                // is there enough stock?
-                console.log("Wants to buy " + response.buyQuantity + " of Item ID " + response.buyID + ".");
-                // console.log("There are " + data[response.buyID].stock_quantity + " in stock.");
-                // TODO: ^^ this doesn't work b/c we're looking at the array entry corresponding with the response.buyID, not the array entry containing an object with the item_ID corresponding to response.buyID.
-                // TODO: could do a second query to the DB, but that seems too.... something.
-                // console.log("response.buyID: " + response.buyID);
-                // console.log("All item IDs:");
-                for (q=0;q<data.length;q++) { // loop through the array of objects
-                    console.log(data[q].item_id);
-                    if (data[q].item_id == response.buyID) { // look for a match between user's ID input and the item_ID of the object
-                    // ^^ if you do a === match, you need to convert the user input into a number
-                        
-                        console.log("found a match!");
-                        if (data[q].stock_quantity < response.buyQuantity) {
-                            console.log("Sorry, we do not have that many in stock.");
-                        } else {
-                            updateQuantity = data[q].stock_quantity - response.buyQuantity;
-                            console.log("updateQuantity: " + updateQuantity);
-                            updateID = response.buyID;
-                            console.log("updateID: " + updateID);
-                            console.log("Sold! You spent $" + (response.buyQuantity * data[q].price).toFixed(2) + ".");
-                            connection.query('update products set stock_quantity = ' + updateQuantity + ' where item_id = ' + updateID, function(err, data) {
-                                if(err) throw err;
-                                console.log("Item quantity updated.");
-                                connection.end();
-                                inquirer.prompt (
-                                    {
-                                        type: "list",
-                                        message: "Would you like to keep shopping?",
-                                        choices: ['Yes','No'],
-                                        name: "keepShopping"
-                                    }
-                                ).then(function(response){
-                                    if(response.keepShopping === 'Yes') {
-                                        console.log("Need to add logic to re-run the prompts and query.")
-                                    } else {
-                                        console.log("We're all done then! Thank you and come again!");
-                                    }
-                                });
+            }
+        };
+        tableData.push([
+            chalk.yellow.bold("ID"),
+            chalk.yellow.bold("Product"),
+            chalk.yellow.bold("Department"),
+            chalk.yellow.bold("In Stock"),
+            chalk.yellow.bold("Price")
+        ]);
+        for (i=0;i<data.length;i++) {
+            var newArray = [
+                data[i].item_id,
+                data[i].product_name,
+                data[i].department_name,
+                data[i].stock_quantity,
+                "$"+data[i].price.toFixed(2)
+            ];
+            tableData.push(newArray);
+            // add all the item numbers into a separate, easily searchable array for user input validation purposes
+            availableItems.push(data[i].item_id);
+        }
+        output = table(tableData,config);
+        console.log(output);
+        // get user input for item and quantity to purchase
+        inquirer.prompt([
+            {
+                type: "number",
+                message: chalk.green("Enter the Item ID of the product you would like to purchase."),
+                name: "buyID",
+                validate: function (buyID) {
+                    if (availableItems.includes(buyID)) { // make sure the itemID they input actually exists in the list of items
+                        return true;
+                    } else {
+                        return "Please enter a valid Item ID."
+                    }
+                }
+                // ^^ validation starting point courtesy of https://github.com/sameeri/Code-Inquirer/wiki/Asking-questions-away-with-Inquirer!
+            },
+            {
+                type: "input",
+                message: chalk.green("How many would you like to purchase?"),
+                name: "buyQuantity",
+                validate: function (buyQuantity) { // make sure the quantity entered is actually a number
+                    var isValid = !isNaN(parseInt(buyQuantity));
+                    return isValid || "Quantity should be a number!";
+                }
+            }
+        ]).then(function(response) {
+            // is there enough stock?
+            for (q=0;q<data.length;q++) { // loop through the array of objects
+                if (data[q].item_id == response.buyID) { // look for a match between user's ID input and the item_ID of the object
+                // ^^ if you do a === match, you need to convert the user input into a number
+                    if (data[q].stock_quantity < response.buyQuantity) { // if stock is too low, display this message:
+                        console.log("\nSorry, we do not have that many in stock.\n");
+                    } else { // otherwise, we assume we have enough stock and proceed.
+                        updateQuantity = data[q].stock_quantity - response.buyQuantity; // what the new quantity should be for this item after the purchase
+                        // confirm to the user:
+                        console.log("\nSold! You spent $" + (response.buyQuantity * data[q].price).toFixed(2) + ".");
+                        console.log("\nThere are " + updateQuantity + " of this item left for purchase.\n")
+                        // update the DB to decrement the inventory:
+                        connection.query('update products set stock_quantity = ? where item_id = ?', [updateQuantity,response.buyID], function(err) {
+                            if(err) throw err;
+                            inquirer.prompt (
+                                {
+                                    type: "list",
+                                    message: "Would you like to keep shopping?",
+                                    choices: ['Yes','No'],
+                                    name: "keepShopping"
+                                }
+                            ).then(function(response){
+                                if(response.keepShopping === 'Yes') {
+                                    // relaunch customer()
+                                    customer();
+                                } else {
+                                    connection.end();
+                                    console.log("\nWe're all done then! Thank you and come again!\n");
+                                }
                             });
-                            
-                        }
-                    } 
-                }
-                
-            });
-            
-        }); // close connection.query
-
-    }); // close connection.connect
-
-
+                        }); // closing connection.query for updating stock
+                    }
+                } 
+            }
+        });
+    }); // close connection.query (select * from products)
 }; // close customer()
-
 // =============================================
 
+
 // =============================================
-// CALL customer() ON LOAD
-customer();
+// CONNECT TO DB AND CALL customer() ON LOAD
+connection.connect(function(err){
+    if(err) throw err;
+    customer()
+});
 // =============================================
 
 
