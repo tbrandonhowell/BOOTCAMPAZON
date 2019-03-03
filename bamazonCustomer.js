859a92e26b5ea1e3c6466d7b193738fb7eb3082a
Brandon H <45020672+tbrandonhowell@users.noreply.github.com>
2019-03-02 21:40:22 -0500

    20190302 pm

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // TIME SPENT:
 // 3/1 2:15
 // 3/2 0:50
+// 3/2 
 
 
 // 5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
@@ -26,21 +27,25 @@ X - inquire "what product would you like to buy"
 X - inquire "how many would you like to buy"
 X - compare # ordered against quantity in the object
 X - logic for having enough quantity or not
-TODO: 8 - update db to remove quantity
+X - update db to remove quantity
 X - show total cost of purchase
 X - ask whether they want to keep shopping
 TODO: 11 - exit or reload the inquirer trigger
+TODO: give them a way to get out of the app by entering Q
 */
 
 
 // TODO: need to have some sort of data input validation happening
+// TODO: ALSO set my queries to sanitize user input
 
 // =============================================
 // NPM PACKAGES
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const pad = require('pad');
-// TODO: should I remove npm pad from this project?
+// const table = require("table");
+const {table} = require('table');
+// ^^ not sure why this works but the normal require doesn't
+const chalk = require('chalk');
 // =============================================
 
 
@@ -51,9 +56,8 @@ var updateID;
 // =============================================
 
 
-// // =============================================
-// CONNECT TO DB, GET PRODUCTS
-// set connection object
+// =============================================
+// CREATE CONNECTION OBJECT
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -61,90 +65,119 @@ var connection = mysql.createConnection({
     password: "uncb00tc4mp",
     database: "bamazon"
 });
-// actually connect
-connection.connect(function(err){
-    if(err) throw err;
-    console.log("connction id: " + connection.threadId);
-    // run your query
-    connection.query('select * from products', function(err, data) {
-        if (err) throw err;
-        // display the data
-        console.log(data);
-        // TODO: why does a FOREACH not work here?
-        // data.foreach(function(item){
-        //     console.log(item.item_id);
-        //     console.log(item.product_name);
-        // });
-        // TODO: ^^^ why does a FOREACH not work here?
-        console.log("\n\nAVAILABLE ITEMS:")
-        console.log("\nItem ID | Name | Category | Price")
-        for (i=0;i<data.length;i++) {
-            console.log(`${data[i].item_id} | ${data[i].product_name} | ${data[i].department_name} | $${data[i].price.toFixed(2)}`);
-        }
-        console.log("");
-        // get user input
-        inquirer.prompt([
-            {
-                // TODO: can inquirer be setup to only accept numbers?
-                type: "input",
-                message: "Enter the Item ID of the product you would like to purchase.",
-                name: "buyID"
-            },
-            {
-                type: "input",
-                message: "How many would you like to purchase?",
-                name: "buyQuantity"
-            }
-        ]).then(function(response) {
-            // is there enough stock?
-            console.log("Wants to buy " + response.buyQuantity + " of Item ID " + response.buyID + ".");
-            // console.log("There are " + data[response.buyID].stock_quantity + " in stock.");
-            // TODO: ^^ this doesn't work b/c we're looking at the array entry corresponding with the response.buyID, not the array entry containing an object with the item_ID corresponding to response.buyID.
-            // TODO: could do a second query to the DB, but that seems too.... something.
-            // console.log("response.buyID: " + response.buyID);
-            // console.log("All item IDs:");
-            for (q=0;q<data.length;q++) { // loop through the array of objects
-                console.log(data[q].item_id);
-                if (data[q].item_id == response.buyID) { // look for a match between user's ID input and the item_ID of the object
-                // ^^ if you do a === match, you need to convert the user input into a number
-                    
-                    console.log("found a match!");
-                    if (data[q].stock_quantity < response.buyQuantity) {
-                        console.log("Sorry, we do not have that many in stock.");
-                    } else {
-                        updateQuantity = data[q].stock_quantity - response.buyQuantity;
-                        console.log("updateQuantity: " + updateQuantity);
-                        updateID = response.buyID;
-                        console.log("updateID: " + updateID);
-                        console.log("Sold! You spent $" + (response.buyQuantity * data[q].price).toFixed(2) + ".");
-                        connection.query('update products set stock_quantity = ' + updateQuantity + ' where item_id = ' + updateID, function(err, data) {
-                            if(err) throw err;
-                            console.log("Item quantity updated.");
-                            connection.end();
-                            inquirer.prompt (
-                                {
-                                    type: "list",
-                                    message: "Would you like to keep shopping?",
-                                    choices: ['Yes','No'],
-                                    name: "keepShopping"
-                                }
-                            ).then(function(response){
-                                if(response.keepShopping === 'Yes') {
-                                    console.log("Need to add logic to re-run the prompts and query.")
-                                } else {
-                                    console.log("We're all done then! Thank you and come again!");
-                                }
-                            });
-                        });
-                        
-                    }
-                } 
-            }
-            
-        });
-        
-    }); // close connection.query
-
-}); // close connection.connect
 // =============================================
+
+
+// =============================================
+// customer() FUNCTION
+var customer = function() {
+    // connect to DB
+    connection.connect(function(err){
+        if(err) throw err;
+        // run our query
+        connection.query('select * from products', function(err, data) {
+            if (err) throw err;
+            console.log(chalk.red.bold("\n============================================="));
+            console.log(chalk.red.bold("||          WELCOME TO BOOTCAMPAZON        ||"));
+            console.log(chalk.red.bold("=============================================\n"));
+            // display the data w/ table package
+            let output;
+            let tableData = [];
+            tableData.push([
+                chalk.yellow.bold("ID"),
+                chalk.yellow.bold("Product"),
+                chalk.yellow.bold("Department"),
+                chalk.yellow.bold("In Stock"),
+                chalk.yellow.bold("Price")
+            ]);
+            for (i=0;i<data.length;i++) {
+                var newArray = [
+                    data[i].item_id,
+                    data[i].product_name,
+                    data[i].department_name,
+                    data[i].stock_quantity,
+                    data[i].price.toFixed(2)
+                ];
+                tableData.push(newArray);
+            }
+            output = table(tableData);
+            console.log(output);
+            // get user input
+            inquirer.prompt([
+                {
+                    type: "number",
+                    message: "Enter the Item ID of the product you would like to purchase.",
+                    name: "buyID",
+                    // validate: function validateInt(input) {
+
+                    // }
+                },
+                {
+                    type: "input",
+                    message: "How many would you like to purchase?",
+                    name: "buyQuantity"
+                }
+            ]).then(function(response) {
+                // is there enough stock?
+                console.log("Wants to buy " + response.buyQuantity + " of Item ID " + response.buyID + ".");
+                // console.log("There are " + data[response.buyID].stock_quantity + " in stock.");
+                // TODO: ^^ this doesn't work b/c we're looking at the array entry corresponding with the response.buyID, not the array entry containing an object with the item_ID corresponding to response.buyID.
+                // TODO: could do a second query to the DB, but that seems too.... something.
+                // console.log("response.buyID: " + response.buyID);
+                // console.log("All item IDs:");
+                for (q=0;q<data.length;q++) { // loop through the array of objects
+                    console.log(data[q].item_id);
+                    if (data[q].item_id == response.buyID) { // look for a match between user's ID input and the item_ID of the object
+                    // ^^ if you do a === match, you need to convert the user input into a number
+                        
+                        console.log("found a match!");
+                        if (data[q].stock_quantity < response.buyQuantity) {
+                            console.log("Sorry, we do not have that many in stock.");
+                        } else {
+                            updateQuantity = data[q].stock_quantity - response.buyQuantity;
+                            console.log("updateQuantity: " + updateQuantity);
+                            updateID = response.buyID;
+                            console.log("updateID: " + updateID);
+                            console.log("Sold! You spent $" + (response.buyQuantity * data[q].price).toFixed(2) + ".");
+                            connection.query('update products set stock_quantity = ' + updateQuantity + ' where item_id = ' + updateID, function(err, data) {
+                                if(err) throw err;
+                                console.log("Item quantity updated.");
+                                connection.end();
+                                inquirer.prompt (
+                                    {
+                                        type: "list",
+                                        message: "Would you like to keep shopping?",
+                                        choices: ['Yes','No'],
+                                        name: "keepShopping"
+                                    }
+                                ).then(function(response){
+                                    if(response.keepShopping === 'Yes') {
+                                        console.log("Need to add logic to re-run the prompts and query.")
+                                    } else {
+                                        console.log("We're all done then! Thank you and come again!");
+                                    }
+                                });
+                            });
+                            
+                        }
+                    } 
+                }
+                
+            });
+            
+        }); // close connection.query
+
+    }); // close connection.connect
+
+
+}; // close customer()
+
+// =============================================
+
+// =============================================
+// CALL customer() ON LOAD
+customer();
+// =============================================
+
+
 
